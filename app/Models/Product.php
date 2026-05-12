df199ecef42609dbb62dc8b9543905f98b913ab3
yusqohid <yusqo975@gmail.com>
2026-05-13 00:19:16 +0700

    category & product model,migration

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'image',
+        'price',
+        'stock',
+        'category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
