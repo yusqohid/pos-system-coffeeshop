ef383ef9487a02ca1290f9aa3d3a2a60e1b25b62
yusqohid <yusqo975@gmail.com>
2026-07-09 16:37:49 +0700

    customer init

---
+++ app/Filament/Resources/Customers/Schemas/CustomerForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Customers\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class CustomerForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('phone')
+                    ->tel()
+                    ->required(),
+                Textarea::make('address')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+}
