4d57195f798aaa0c18781e6a44f7c722dd6073fe
yusqohid <yusqo975@gmail.com>
2026-07-09 18:14:28 +0700

    create order model&migration, change timezone

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
@@ -11,4 +12,9 @@ class Customer extends Model
         'phone',
         'address',
     ];
+
+    public function order(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
---
+++ app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'customer_id',
+        'total_price',
+        'date',
+    ];
+
+    public function customer():BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
